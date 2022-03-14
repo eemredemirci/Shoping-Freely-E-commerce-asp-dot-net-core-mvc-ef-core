55c197e719967c42922672ac1b877b987e55eda8
Emre Demirci <eemredemirci@gmail.com>
2022-03-14 22:53:57 +0300

    UI.MVC

---
+++ Shopping-Freely-ECommerce/ShoppingFreely.UI.MVC/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingFreely.UI.MVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
